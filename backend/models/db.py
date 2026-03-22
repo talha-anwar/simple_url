e0f7a0e3c5f521158c1f4e35de7e31b80702e9d3
talha-anwar <talhaibnanwar@gmail.com>
2026-03-23 03:23:13 +0600

    add docker setup

--- backend/models/db.py
+++ backend/models/db.py
@@ -1,9 +1,15 @@
+import os
+from urllib.parse import quote_plus
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:easypass%40shorter_url@localhost/url_db"
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", "easypass@shorter_url"))
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "url_db")
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
